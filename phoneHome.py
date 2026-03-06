380a481bf670024aeeb5ec8e1098d53e2bbf2060
RakaAmburo <raka.amburo@gmail.com>
2026-03-06 17:33:58 +0100

    adding ping flag

--- phoneHome.py
+++ phoneHome.py
@@ -10,6 +10,7 @@ PHONE_MAC = os.getenv("PHONE_MAC")
 PHONE_IP = os.getenv("PHONE_IP")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+PING_FLAG = os.getenv("PING_FLAG", "-n")# win default
 
 INTERVAL = 60  # seconds
 
@@ -19,8 +20,7 @@ def send_telegram(message):
 
 def is_home(retries=3, delay=5):
     for attempt in range(retries):
-        #subprocess.run(['ping', '-n', '1', PHONE_IP], capture_output=True)
-        subprocess.run(['ping', '-c', '1', PHONE_IP], capture_output=True)
+        subprocess.run(['ping', PING_FLAG, '1', PHONE_IP], capture_output=True)
         result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
         print(result.stdout)
         if PHONE_MAC.lower() in result.stdout.lower():
