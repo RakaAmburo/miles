65aadf229f1d3a69e45fbed2da4e04fae5585b0f
RakaAmburo <raka.amburo@gmail.com>
2026-03-06 19:59:21 +0100

    adding config comments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,3 +7,20 @@ module.exports = {
     }
   ]
 }
+
+/* module.exports = {
+  apps: [
+    {
+      name: "phone-tracker",
+      script: "phoneHome.py",
+      interpreter: "/home/pablo/repos/miles/.venv/bin/python3"
+    },
+    {
+      name: "invoice-reader",
+      script: "invoiceReader.py",
+      interpreter: "/home/pablo/repos/miles/.venv/bin/python3"
+    }
+  ]
+}
+
+bashpm2 restart ecosystem.config.js */
